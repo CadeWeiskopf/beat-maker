634a7602ac40a2e305e1615fd7687d974d44ff6d
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-07-27 22:14:09 -0400

    comments

--- src/components/BeatPad.tsx
+++ src/components/BeatPad.tsx
@@ -1,13 +1,13 @@
 // In BeatPad.tsx
 import React, { useCallback, useEffect, useState } from "react";
 
-const NUM_STEPS = 8; // Number of steps in the musical sequence
+const NUM_STEPS = 8;
 
 const BeatPad: React.FC = () => {
   const [sequence, setSequence] = useState<boolean[]>(
     new Array(NUM_STEPS).fill(false)
   );
-  const [currentBeat, setCurrentBeat] = useState<number>(0); // Current beat index
+  const [currentBeat, setCurrentBeat] = useState<number>(0);
   const [tempoBeat, setTempoBeat] = useState<number>(0);
 
   const toggleStep = (step: number) => {
@@ -23,9 +23,7 @@ const BeatPad: React.FC = () => {
   };
 
   const playSequence = useCallback(() => {
-    // Here, you can play the audio for the active beats (e.g., kicks and snares)
-    // For simplicity, we will just log the active steps for now.
-    const additionalBeats = 4; // Number of additional beats per tempo interval
+    const additionalBeats = 4;
     for (let i = 0; i < additionalBeats; i++) {
       setTimeout(() => {
         setCurrentBeat((prevBeat) => (prevBeat + 1) % NUM_STEPS);
@@ -36,7 +34,7 @@ const BeatPad: React.FC = () => {
   }, [sequence]);
 
   useEffect(() => {
-    const tempoInterval = (60 / tempo) * 1000; // Calculate the interval based on tempo
+    const tempoInterval = (60 / tempo) * 1000;
     const tempoTimer = setInterval(playSequence, tempoInterval);
     return () => clearInterval(tempoTimer);
   }, [tempo, sequence, playSequence]);
