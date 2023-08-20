0ca92e7909f56c5845846b52cd76312d6fe09317
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-08-19 21:43:56 -0400

    restart with webworkers

--- src/components/BeatPad.tsx
+++ src/components/BeatPad.tsx
@@ -1,97 +1,82 @@
-import React, { useCallback, useEffect, useState } from "react";
-
-const NUM_STEPS = 8;
-const METRONOME_NUMBER = 2;
-const BEATS_PER_TICK = 4;
+import React, { useState, useEffect } from "react";
 
 const BeatPad: React.FC = () => {
-  const [sequence, setSequence] = useState<boolean[]>(
-    new Array(NUM_STEPS).fill(false)
-  );
-  const [metronome, setMetronome] = useState<boolean[]>(
-    new Array(METRONOME_NUMBER).fill(false)
-  );
-  const [currentBeat, setCurrentBeat] = useState<number>(0);
-  const [tempoBeat, setTempoBeat] = useState<number>(0);
+  const BPM = 120;
+  const beatsBetween = 4;
+  const totalBeats = 8;
 
-  const toggleStep = (step: number) => {
-    console.log("toggle", step, sequence[step]);
-    sequence[step] = !sequence[step];
-    setSequence(sequence.map((s) => s));
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+  const [isStarted, setIsStarted] = useState(false);
+  let beatStep = 0;
+  const [kickSequence, setKickSequence] = useState([
+    true,
+    false,
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+  ]);
+  const [snareSequence, setSnareSequence] = useState([
+    false,
+    false,
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+  ]);
+
+  const startAudioContext = () => {
+    const context = new AudioContext();
+    setAudioContext(context);
+    setIsStarted(true);
   };
-
-  const [tempo, setTempo] = useState<number>(120);
-
-  const handleTempoChange = (newTempo: number) => {
-    setTempo(newTempo);
-  };
-
-  const playSequence = useCallback(() => {
-    setTempoBeat((prevBeat) => (prevBeat + 1) % METRONOME_NUMBER);
-    console.log(sequence);
-  }, [sequence]);
-
-  const playBeats = useCallback(() => {
-    setCurrentBeat((prevBeat) => (prevBeat + 1) % NUM_STEPS);
-    console.log(sequence);
-  }, [sequence]);
 
   useEffect(() => {
-    const tempoInterval = (60 / tempo) * 1000;
-    const beatsInterval = (60 / tempo) * (1 / BEATS_PER_TICK) * 1000;
-    const tempoTimer = setInterval(playSequence, tempoInterval);
-    const beatsTimer = setInterval(playBeats, beatsInterval);
-    return () => {
-      clearInterval(tempoTimer);
-      clearInterval(beatsTimer);
-    };
-  }, [tempo, sequence, playSequence, playBeats]);
+    if (isStarted) {
+      const syncWorker = new Worker("worker.js");
+      const beatInterval = 60000 / BPM / beatsBetween;
+      let nextBeatTime = audioContext?.currentTime || 0;
+
+      const scheduleBeat = (beatType: string) => {
+        syncWorker.postMessage({ beatType });
+      };
+
+      let offset = 0;
+      const scheduleNextBeat = () => {
+        nextBeatTime += beatInterval / 1000;
+        if (kickSequence[beatStep]) {
+          scheduleBeat("kick");
+        }
+        if (snareSequence[beatStep]) {
+          scheduleBeat("snare");
+        }
+        beatStep++;
+        if (beatStep >= totalBeats) {
+          beatStep = 0;
+        }
+        // scheduleBeat("kick");
+        // // TODO: remove this, actually build a sequence
+        // if (offset++ % 2 !== 0) {
+        //   scheduleBeat("snare");
+        // }
+      };
+
+      const interval = setInterval(scheduleNextBeat, beatInterval);
+
+      return () => {
+        clearInterval(interval);
+        audioContext?.close();
+      };
+    }
+  }, [isStarted, audioContext]);
 
   return (
-    <div className="beat-pad">
-      <div style={{ display: "flex" }}>
-        {metronome.map((isMetronome, step) => {
-          return (
-            <div key={step}>
-              <button
-                disabled
-                className={`beat-pad-step ${
-                  step === tempoBeat ? "active-tempo-beat" : ""
-                }`}
-              />
-              {new Array(BEATS_PER_TICK - 1).fill(null).map((e, i) => {
-                return (
-                  <button
-                    key={i}
-                    disabled
-                    className={`beat-pad-step`}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        {sequence.map((isBeat, step) => {
-          return (
-            <button
-              key={step}
-              className={`beat-pad-step ${isBeat ? "active" : ""} ${
-                currentBeat === step ? "active-tempo-beat" : ""
-              }`}
-              onClick={() => toggleStep(step)}
-            />
-          );
-        })}
-      </div>
-      <input
-        type="number"
-        disabled
-        onInput={(event: React.FormEvent<HTMLInputElement>) => {
-          handleTempoChange(event.currentTarget.valueAsNumber);
-        }}
-      />
+    <div>
+      {!isStarted && <button onClick={startAudioContext}>Start</button>}
     </div>
   );
 };
